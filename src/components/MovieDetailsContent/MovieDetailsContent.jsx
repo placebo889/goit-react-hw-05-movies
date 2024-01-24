ae5ffd5a4bd4b21b328ca9d714967b57f8431391
placebo889 <hentalia2d@gmail.com>
2024-01-24 20:42:11 +0200

    Update MovieDetailsContent.jsx

--- src/components/MovieDetailsContent/MovieDetailsContent.jsx
+++ src/components/MovieDetailsContent/MovieDetailsContent.jsx
@@ -7,7 +7,9 @@ import Loader from 'components/Loader/Loader';
 const MovieDetailsContent = ({ details }) => {
   const { title, poster_path, vote_average, overview, genres } = details;
   const userRate = (vote_average * 10).toFixed(1);
-  const image = 'https://image.tmdb.org/t/p/w500' + poster_path;
+  const BASE_IMG = 'https://image.tmdb.org/t/p/w500';
+  const DEFAULT_IMG = 'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
+  const image = poster_path ? BASE_IMG + poster_path : DEFAULT_IMG;
 
   return (
     <Suspense fallback={<Loader />}>
